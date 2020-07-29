7d002e049a0c018104af89c0173cd99cbc310f57
Bruno Pastre <pastre68@gmail.com>
2020-07-29 04:34:51 -0300

    Calls flutter code from swift! Devices are capable of updating now

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,17 +7,10 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
     let _ = HomeKitWrapper.instance
-  let CHANNEL_NAME = "co.getaether/getaether-flutter"
-    let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-    let channel = FlutterMethodChannel(name: CHANNEL_NAME, binaryMessenger: controller.binaryMessenger)
-
-  channel.setMethodCallHandler { [unowned self] (call, result) in
-    switch call.method {
-    case "getDetails": result(HomeKitWrapper.instance.getTransformedHomes())
-         default: result("Deu ruim pai")
-     }
-  }
+    
+    ChannelWrapper.instance.load(binaryMessenger: (window.rootViewController as! FlutterViewController).binaryMessenger )
 
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
--- lib/integration/homekit/HomeKitChannelManager.dart
+++ lib/integration/homekit/HomeKitChannelManager.dart
@@ -6,6 +6,19 @@ class HomeKitChannelManager {
   static const String CHANNEL_NAME = "co.getaether/getaether-flutter";
   MethodChannel platform =  MethodChannel(CHANNEL_NAME);
 
+
+  HomeKitChannelManager() {
+    platform.setMethodCallHandler((call) {
+      switch(call.method) {
+        case "onDevicesChanged":
+          print("AE BROW");
+          this.getDetails().then((value) => print("Updated to $value"));
+          break;
+        default: print("CUSAO");
+      }
+    });
+  }
+
   Future<String> getDetails() async {
     try {
       // WARNING do not change this unless you change it in AppDelegate.swift as well!!
